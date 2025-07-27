7bdf1badc28aaeb1c53f84f91d6460e40e697494
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-27 19:33:27 -0300

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,9 +1,4 @@
-return function(...)
-    local info = debug.getinfo(2)
-    if info and info.name == nil and info.what == "Lua" and info.source == "=[C]" then
-        -- Ejecutado desde variable, no permitido
-        error("❌ Esta función no puede ser almacenada en una variable.")
-    end
-
-    print("✅ Ejecutado correctamente con parámetro:", ...)
-end
+-- Test.lua
+(function(...)
+    print("✅ Ejecutado directamente con argumento:", ...)
+end)(...)
